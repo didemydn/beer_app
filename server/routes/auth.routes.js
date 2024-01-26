7965951802fb2a2704b81c69cff64ab480585c25
Didem Cakir <didemydn@gmail.com>
2024-01-26 22:34:20 +0100

    server

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User.model");
+
+const router = express.Router();
+const saltRounds = 10;
+
+//POST /user/signup
+
+//POST /user/login
+
+//POST /user/verify
+
+
+modele.exports = router; 

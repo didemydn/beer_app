351776ac80eb2cc8b1567454abd66a509ec055b5
Didem Cakir <didemydn@gmail.com>
2024-02-14 12:53:41 +0100

    search update

--- client/src/pages/SearchPage/Search.js
+++ client/src/pages/SearchPage/Search.js
@@ -1,16 +1,48 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
+import beerAppService from "../../services/beerApp.service";
 
-function Search({onSearch}) {
-    const [searchQuery, setSearchQuery] = useState('');
-    const handleSearch = () => {
-        onSearch(searchQuery);
-    };
+function Search(e) {
+    const [input, setInput] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [selectedBeers, setSelectedBeers] = useState(null);
+
+    useEffect(()=>{
+        if (input.trim() !== '') {
+        beerAppService.getAllBeers(input)
+            .then((response) => {
+                setSearchResults(response.data);
+            })
+            .catch((error) => {
+                console.log("Error searching beers:", error)
+            });
+        } else {
+            setSearchResults([]);
+        }
+    }, [input]);
+
+    const handleInputChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    const handleAddToList = (beer) => {
+        setSelectedBeers(beer);     
+         
+        } 
+    
 
     return(
         <div>
             <h2>Search beers and rate</h2>
-            <input type="text" placeholder="Search for beers" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
-            <button onClick={handleSearch}>Search</button>
+            <input type="text" placeholder="Search for beers" value={input} onChange={handleInputChange}/>
+            <ul>
+                {searchResults.map((beer, index) => (
+                    <li key={index}>
+                        {beer.name} 
+                        <button onClick={() => handleAddToList(beer)}>Add to List</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
